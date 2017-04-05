494ff1ceeebc85afe4d7b5faf940722b65d863d9
vohoangnhut <nhut.vo@dou.com.vn>
2017-04-05 15:05:13 +0700

    audit source

--- js/formValidation.js
+++ js/formValidation.js
@@ -11,20 +11,12 @@ $('#btnA').click(function() {
 
 });
 
-$('#btnB').click(function() {
 
-	swal({
-		  title: 'Error!',
-		  text: 'Do you want to continue',
-		  type: 'error',
-		  confirmButtonText: 'Cool'
-		})
-});
 
 const toggleAlert = () => {
 	//alert msg content : <strong>Warning!</strong> Bla Bla
 	if ( $( ".alert-validation" ).is( ":hidden" ) ) {
-	    $( ".alert-validation" ).slideDown(400, ()=>{setTimeout( ()=>{$( ".alert-validation" ).slideUp(400)} , 5000)})
+	    $( ".alert-validation" ).slideDown(400, ()=>{setTimeout( ()=>{$( ".alert-validation" ).slideUp(400)} , 4000)})
 	} 
 	// else {
 	//     $( ".alert-validation" ).slideUp(400)
@@ -35,3 +27,46 @@ const toggleAlert = () => {
 $('#btnCloseAlert').click(function(e) {
 	  $(this).parent().slideUp(300);
 })
+
+
+//Sweet Alert
+$('#btnB').click(function() {
+	swal({
+  title: 'Input something',
+  input: 'text',
+  showCancelButton: true,
+  inputValidator: function (value) {
+    return new Promise(function (resolve, reject) {
+      if (value) {
+        resolve()
+      } else {
+        reject('You need to write something!')
+      }
+    })
+  }
+}).then(function (result) {
+  swal({
+    type: 'success',
+    html: 'You entered: ' + result
+  })
+})
+
+	// swal({
+	// 	  title: 'Error!',
+	// 	  text: 'Do you want to continue',
+	// 	  type: 'error', //question, success, warning
+	// 	  showCancelButton: true,
+	// 	  cancelButtonColor: '#d33',
+	// 	  //cancelButtonText: 'abc', Default is Cancel
+	// 		confirmButtonColor: '#3085d6',	
+	// 		confirmButtonText: 'Yes, delete it!',
+	// 	  //timer : 2000 //auto close in 2000 milisecond
+	// 	}).then(
+	// 		()=>{swal('Deleted!','Your file has been deleted.','success')},
+
+	// 		(dismiss)=>{
+	// 			// dismiss can be 'cancel', 'overlay',
+  	// 			// 'close', and 'timer'
+	// 			  if (dismiss == 'cancel') swal('error!','Your file has been deleted.','error')}
+	// 	)
+});
